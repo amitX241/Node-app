72edfa6a243e88f99aa0972bf564568ece949f8a
amitX241 <2022sp93059@wilp.bits-pilani.ac.in>
2023-05-05 11:18:02 +0530

    New Route for home added

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const app = require('express')();
 
 const PORT = 3000;
 
+app.get('/', (req, res) => {
+    console.log('HOME');
+})
+
 app.listen(PORT, () => {
     console.log('listening on port', PORT);
 });
